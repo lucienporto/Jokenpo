5292d763791be87e40fb0214f46962a2257a09f7
Lucien Porto <136341310+lucienporto@users.noreply.github.com>
2024-01-29 11:52:28 -0300

    basic game
    creating the basic logic

--- program/Program.cs
+++ program/Program.cs
@@ -1,9 +1,89 @@
-Console.WriteLine("Insira o lado A: ");
-            double a = Convert.ToDouble(Console.ReadLine());
-            
-            Console.WriteLine("Insira o lado B: ");
-            double b = Convert.ToDouble(Console.ReadLine());
+//string play = "";
+string? readResult;
+string jokenpo;
+string selected = "";
 
-            double c = Math.Sqrt((a * a) + (b * b));
+Random random = new Random();
+int dice = random.Next(1, 4);
 
-            Console.WriteLine($"A hipotenusa é {c}.");
+if (dice == 1)
+{
+    jokenpo = "Pedra";
+}
+else if (dice == 2)
+{
+    jokenpo = "Papel";
+}
+else
+{
+    jokenpo = "Tesoura";
+}
+
+// Interação básica
+Console.Clear();
+    
+Console.WriteLine("=============");
+Console.WriteLine("| JO-KEN-PO |");
+Console.WriteLine("=============");
+Console.WriteLine("Escolha pedra, papel ou tesoura:");
+Console.WriteLine("1. Pedra");
+Console.WriteLine("2. Papel");
+Console.WriteLine("3. Tesoura");
+readResult = Console.ReadLine();
+if (readResult != null)
+{
+    selected = readResult.ToLower();
+}
+
+switch (selected)
+{
+    case "1":
+        Console.WriteLine("Você escolheu: Pedra");
+        if (jokenpo == "Pedra")
+        {
+            Console.WriteLine("Empate!");
+        }
+        else if (jokenpo == "Tesoura")
+        {
+            Console.WriteLine("Você ganhou!");
+        }
+        else if (jokenpo == "Papel")
+        {
+            Console.WriteLine("Você perdeu!");
+        }
+        break;
+
+    case "2":
+        Console.WriteLine("Você escolheu: Papel");
+        if (jokenpo == "Papel")
+        {
+            Console.WriteLine("Empate!");
+        }
+        else if (jokenpo == "Pedra")
+        {
+            Console.WriteLine("Você ganhou!");
+        }
+        else if (jokenpo == "Tesoura")
+        {
+            Console.WriteLine("Você perdeu!");
+        }
+        break;
+
+    case "3":
+        Console.WriteLine("Você escolheu: Tesoura");
+        if (jokenpo == "Tesoura")
+        {
+            Console.WriteLine("Empate!");
+        }
+        else if (jokenpo == "Papel")
+        {
+            Console.WriteLine("Você ganhou!");
+        }
+        else if (jokenpo == "Pedra")
+        {
+            Console.WriteLine("Você perdeu!");
+        }
+        break;
+}
+
+Console.WriteLine("Obrigado por jogar!");
--- program/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ program/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380aa60014597e2c01ad8f34453164359ded5ccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
